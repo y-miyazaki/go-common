c60784a53360d86e7b936cabe7a28d70b27c0ade
Yoshiaki MIYAZAKI(JP) <ymiyazakixyz@gmail.com>
2022-02-23 17:08:11 +0000

    fixed.

---
+++ example/gin_cors1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/y-miyazaki/go-common/pkg/middleware"
+)
+
+func main() {
+	router := gin.Default()
+	// CORS for https://foo.com and https://github.com origins, allowing:
+	// - PUT and PATCH methods
+	// - Origin header
+	// - Credentials share
+	// - Preflight requests cached for 12 hours
+	router.Use(middleware.GinHTTPHeaderAccessControlAllow(
+		&middleware.GinHTTPHeaderAccessControlConfigSetting{
+			AllowAllOrigins:  false,
+			AllowOrigins:     []string{"https://foo.com"},
+			AllowMethods:     []string{"PUT", "PATCH"},
+			AllowHeaders:     []string{"Origin"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		}))
+	router.Run()
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/aws/aws-sdk-go v1.43.3
 	github.com/fsnotify/fsnotify v1.5.1
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.7
 	github.com/nlopes/slack v0.6.0
 	github.com/pkg/errors v0.9.1
