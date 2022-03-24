e2ffa0589cb64862666c934cda89c157277af900
Yoshiaki MIYAZAKI(JP) <ymiyazakixyz@gmail.com>
2022-03-24 18:04:20 +0000

    fixed.

--- pkg/utils/db.go
+++ pkg/utils/db.go
@@ -5,9 +5,9 @@ import "strings"
 // GetMySQLDsn creates a MySQL DSN string.
 // about DSN document.
 // https://github.com/go-sql-driver/mysql#dsn-data-source-name
-func GetMySQLDsn(username, password, server, port, dbname, parameter string) string {
+func GetMySQLDsn(username, password, server, port, db, parameter string) string {
 	if !strings.HasPrefix(parameter, "?") {
 		parameter = "?" + parameter
 	}
-	return username + ":" + password + "@tcp(" + server + ":" + port + ")/" + dbname + parameter
+	return username + ":" + password + "@tcp(" + server + ":" + port + ")/" + db + parameter
 }
