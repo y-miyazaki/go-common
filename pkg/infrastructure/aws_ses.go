1c4b4eca29160b63756f4f5f4848ed23d803466d
Yoshiaki MIYAZAKI(JP) <ymiyazakixyz@gmail.com>
2022-03-30 01:29:50 +0000

    fixed

--- pkg/infrastructure/aws_ses.go
+++ pkg/infrastructure/aws_ses.go
@@ -22,31 +22,39 @@ func NewSES(
 
 // GetSESConfig get config.
 func GetSESConfig(e *logrus.Entry, id, secret, token, region, endpoint string) *aws.Config {
+	var httpClient *http.Client
+	if e != nil {
+		httpClient = &http.Client{
+			Transport: transport.NewTransportHTTPLogger(
+				e.WithField("service", "aws-ses"),
+				transport.TransportHTTPLoggerTypeExternal,
+			),
+		}
+	}
 	return &aws.Config{
 		Credentials: credentials.NewStaticCredentials(
 			id,
 			secret,
 			token),
-		Region: aws.String(region),
-		HTTPClient: &http.Client{
-			Transport: transport.NewTransportHTTPLogger(
-				e.WithField("service", "aws-ses"),
-				transport.TransportHTTPLoggerTypeExternal,
-			),
-		},
+		Region:     aws.String(region),
+		HTTPClient: httpClient,
 	}
 }
 
 // GetSESConfigNoCredentials get no credentials config.
 // If AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are environment variables and are in the execution environment, Credentials is not required.
 func GetSESConfigNoCredentials(e *logrus.Entry, region, endpoint string) *aws.Config {
-	return &aws.Config{
-		Region: aws.String(region),
-		HTTPClient: &http.Client{
+	var httpClient *http.Client
+	if e != nil {
+		httpClient = &http.Client{
 			Transport: transport.NewTransportHTTPLogger(
 				e.WithField("service", "aws-ses"),
 				transport.TransportHTTPLoggerTypeExternal,
 			),
-		},
+		}
+	}
+	return &aws.Config{
+		Region:     aws.String(region),
+		HTTPClient: httpClient,
 	}
 }
