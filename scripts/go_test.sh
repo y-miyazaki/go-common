828548ee5492c3ed7a39a2d0adc418f68283be7d
Yoshiaki MIYAZAKI(JP) <ymiyazakixyz@gmail.com>
2022-04-12 07:11:13 +0000

    fixed.

--- scripts/go_test.sh
+++ scripts/go_test.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-ROOT_DIR=${PWD}
-GIT_DOMAIN=github.com
-
-# set ssh and git config
-echo "Host ${GIT_DOMAIN}\n\tStrictHostKeyChecking no\n\tIdentityFile /root/.ssh/id_rsa\n" >> /root/.ssh/config
-ssh-keyscan -H ${GIT_DOMAIN} >> /root/.ssh/known_hosts
-git config --global url."git@${GIT_DOMAIN}:".insteadOf "https://${GIT_DOMAIN}/"
 
 gvm install go1.16
 gvm use go1.16
